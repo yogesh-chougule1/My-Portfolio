0009ae875d0618a0761bb357adc742e039c6f81c
AzureAD\YogeshChougule <yogesh.chougule@ibm.com>
2025-03-08 14:51:29 +0530

    minor change

--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -1,8 +1,8 @@
 function Achievements() {
   const achievementsList = [
-    { title: "Men with Perfectionist Award", issuer: "IBM", year: "2024" },
-    { title: "Employee of the Year", issuer: "Pine Labs", year: "2023" },
-    { title: "First Prize in College Tech Events", issuer: "University", year: "2021" }
+    { title: "Men with Perfectionist Award", issuer: "IBM, Mumbai Maharashtra", year: "2024" },
+    { title: "Employee of the Year", issuer: "Pine Labs, Mumbai Maharashtra", year: "2023" },
+    { title: "First Prize in College Tech Events", issuer: "D.G.Ruparel College, Mumbai Maharashtra", year: "2021" }
   ];
 
   return (
