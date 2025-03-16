0699c442ef3846a26d4f80b73601b58607e43b03
AzureAD\YogeshChougule <yogesh.chougule@ibm.com>
2025-03-16 16:09:01 +0530

    footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => (
     <footer className="footer">
-      <p>&copy; 2024 Yogesh Chougule | Senior Software Engineer</p>
+      <p>&copy; 2025 Yogesh Chougule | Senior Software Engineer</p>
     </footer>
   );
   export default Footer;
